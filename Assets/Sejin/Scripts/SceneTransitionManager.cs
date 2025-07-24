1aefedabbc7b92f419b99900a080ed691836e48c
DESKTOP-HRD488S\202_12 <ohyas_44@naver.com>
2025-07-24 18:23:43 +0900

    씬 전환 수정 필요

--- Assets/Sejin/Scripts/SceneTransitionManager.cs
+++ Assets/Sejin/Scripts/SceneTransitionManager.cs
@@ -1,39 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using System.Collections;
 
 public class SceneTransitionManager : MonoBehaviour
 {
     public static SceneTransitionManager Instance { get; private set; }
 
-    [SerializeField] private Image fadeImage;
-    [SerializeField] private float fadeOutDuration = 1f;
-    [SerializeField] private float fadeInDuration = 1f;
-    private bool isFading = false;
-    private Color originalColor;
+    private bool isTransitioning = false;
 
     void Awake()
     {
-        // 싱글톤 패턴
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-
-            // 씬 로드 이벤트 등록
-            SceneManager.sceneLoaded += OnSceneLoaded;
+            SceneManager.sceneLoaded += OnSceneLoaded;  // 씬 로드 완료 이벤트 등록
         }
         else
         {
             Destroy(gameObject);
-            return;
         }
     }
 
     void OnDestroy()
     {
-        // 이벤트 해제
         if (Instance == this)
         {
             SceneManager.sceneLoaded -= OnSceneLoaded;
@@ -42,84 +31,25 @@ public class SceneTransitionManager : MonoBehaviour
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        // "FadeImage" 태그를 가진 오브젝트 찾기
-        GameObject fadeObj = GameObject.FindGameObjectWithTag("FadeImage");
-        if (fadeObj != null)
-        {
-            fadeImage = fadeObj.GetComponent<Image>();
-        }
-        else
-        {
-            Debug.LogError("FadeImage 태그를 가진 오브젝트를 찾을 수 없습니다!");
-            return;
-        }
-
-        if (fadeImage != null)
-        {
-            originalColor = fadeImage.color;
-            fadeImage.color = new Color(originalColor.r, originalColor.g, originalColor.b, 1f);
-            StartCoroutine(FadeIn());
-        }
+        isTransitioning = false;  // 씬이 완전히 로드되면 전환 가능 상태로 변경
     }
 
-    // 사용법: SceneTransitionManager.Instance.FadeOutAndChangeScene(sceneIndex);
-    public void FadeOutAndChangeScene(int sceneIndex)
+    // 메서드 이름 그대로 씬 전환 처리
+    public void ChangeScene(int sceneIndex)
     {
-        if (!isFading)
-            StartCoroutine(FadeOutAndLoadSceneCoroutine(sceneIndex));
+        if (!isTransitioning)
+        {
+            isTransitioning = true;
+            SceneManager.LoadScene(sceneIndex);
+        }
     }
 
-
-    // 페이드 없이 바로 게임 종료
     public void QuitGame()
     {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;  // 에디터에서만 실행
+        UnityEditor.EditorApplication.isPlaying = false;
 #else
-        Application.Quit();  // 빌드된 게임에서는 이것이 실행
+        Application.Quit();
 #endif
     }
-
-    IEnumerator FadeOutAndLoadSceneCoroutine(int targetSceneIndex)
-    {
-        isFading = true;
-        float elapsed = 0f;
-        Color startColor = fadeImage.color;
-        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 1f); // 알파 1(불투명) - 페이드 아웃
-
-        while (elapsed < fadeOutDuration)
-        {
-            float t = elapsed / fadeOutDuration;
-            fadeImage.color = Color.Lerp(startColor, endColor, t);
-            elapsed += Time.unscaledDeltaTime;
-            yield return null;
-        }
-        fadeImage.color = endColor;
-        isFading = false;
-
-        // 전달받은 씬 이름으로 변경
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(targetSceneIndex);
-        yield return new WaitUntil(() => asyncLoad.isDone);
-    }
-
-
-    IEnumerator FadeIn()
-    {
-        Debug.Log("FadeIn called");
-        isFading = true;
-        float elapsed = 0f;
-        Color startColor = fadeImage.color;
-        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 0f); // 알파 0(투명) - 페이드 인
-
-        while (elapsed < fadeInDuration)
-        {
-            float t = elapsed / fadeInDuration;
-            fadeImage.color = Color.Lerp(startColor, endColor, t);
-            elapsed += Time.unscaledDeltaTime;
-            yield return null;
-        }
-        fadeImage.color = endColor;
-        isFading = false;
-    }
-
 }
